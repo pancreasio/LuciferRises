66e3f56da27bcc86e034da76b0dabd934602b41a
pancreasio <julianholisu@gmail.com>
2019-06-10 19:28:15 -0300

    Shielded Azazel Works
    Unleashed Azazel not quite yet

---
+++ Lucifer-Rises/Assets/Scripts/AzazelShielded.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AzazelShielded : MonoBehaviour
+{
+    public Transform protectTarget, playerTransform;
+    private GameObject protectOffsetPosition, playerFollowTransform;
+    public GameObject unleashedForm;
+    public bool shouldProtect = false;
+    public int maxHP;
+    public float speed, protectOffset, followOffset;
+    private int hp;
+
+    private void Start()
+    {
+        hp = maxHP;
+        protectOffsetPosition = new GameObject();
+        playerFollowTransform = new GameObject();
+    }
+
+    private void Update()
+    {
+        if (hp <= 0)
+        {
+            Explode();
+        }
+
+        if (shouldProtect && protectTarget != null)
+        {
+            protectOffsetPosition.transform.position = new Vector3(protectTarget.position.x, protectTarget.position.y - protectOffset, 0f);
+            transform.position += Vector3.Normalize(protectOffsetPosition.transform.position - transform.position) * speed * Time.deltaTime;
+        }
+        else
+        {
+            playerFollowTransform.transform.position = new Vector3(playerTransform.position.x, followOffset, 0f);
+            transform.position += Vector3.Normalize(playerFollowTransform.transform.position - transform.position) * speed * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            Explode();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Bullet")
+        {
+            hp--;
+        }
+    }
+
+    private void Explode()
+    {
+        Instantiate(unleashedForm, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Lucifer-Rises/Assets/Scripts/AzazelUnleashed.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AzazelUnleashed : MonoBehaviour
+{
+    public float speed;
+    public int maxHP;
+    private int hp;
+    public Transform target;
+
+    void Start()
+    {
+        hp = maxHP;
+    }
+
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            Explode();
+        }
+
+        transform.LookAt(target);
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    private void Explode()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            Explode();
+        }
+    }
+}
