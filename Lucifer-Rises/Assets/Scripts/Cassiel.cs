1400235568f9deecb2b4ddb34e0889723d91a138
pancreasio <julianholisu@gmail.com>
2019-06-10 10:24:11 -0300

    Wave and Waypoint systems done
    THERE WILL BE BLOOD!
    SHED!

--- Lucifer-Rises/Assets/Scripts/Cassiel.cs
+++ Lucifer-Rises/Assets/Scripts/Cassiel.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class Cassiel : MonoBehaviour
 {
     public int maxHp;
-    private int hp;
-    public float fireRate;
+    private int hp, waypointPosition;
+    public float fireRate, speed, waypointOffset;
     private float fireClock;
     public GameObject bullet;
     public Transform cannon;
     public List<Transform> waypoints;
+    public Transform endPoint;
+
 
     private void Start()
     {
         hp = maxHp;
         fireClock = 0;
+        waypointPosition = 0;
     }
 
     private void Update()
@@ -32,6 +35,23 @@ public class Cassiel : MonoBehaviour
             Fire();
             fireClock = 0;
         }
+
+        if (waypointPosition >= waypoints.Count)
+        {
+            transform.position += Vector3.Normalize(endPoint.position - transform.position) * speed * Time.deltaTime;
+            if (Vector3.Distance(endPoint.position, transform.position) < waypointOffset)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        else
+        {
+            transform.position += Vector3.Normalize(waypoints[waypointPosition].position - transform.position) * speed * Time.deltaTime;
+            if (Vector3.Distance(waypoints[waypointPosition].transform.position, transform.position) < waypointOffset)
+            {
+                waypointPosition++;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -43,15 +63,13 @@ public class Cassiel : MonoBehaviour
     }
 
     private void OnCollisionEnter(Collision collision)
-    {        
+    {
         Explode();
     }
 
     private void Fire()
     {
         GameObject bulletInstance = Instantiate(bullet, cannon.position, Quaternion.identity);
-        bulletInstance.GetComponent<EnemyBullet>().creator = this.gameObject;
-        bulletInstance.GetComponent<EnemyBullet>().shouldDissapearIfParentKilled = true;
     }
 
     private void Explode()
--- Lucifer-Rises/Assets/Scripts/Wave.cs
+++ Lucifer-Rises/Assets/Scripts/Wave.cs
@@ -4,17 +4,40 @@ using UnityEngine;
 
 public class Wave : MonoBehaviour
 {
-    private List<Cassiel> cassielList;
+    public int enemyCant;
+    private int spawnedEnemies;
+    public float enemyDelay, enemySpeed, enemyAggression;
+    private float delayClock;
     public List<Transform> waypointList;
+    public GameObject cassiel;
+    public Transform spawnPoint, endPoint;
 
 
     private void Start()
     {
-        
+        spawnedEnemies = 0;
+        delayClock = 0;
     }
 
     private void Update()
     {
-        
+        delayClock += Time.deltaTime;
+
+        if (delayClock > enemyDelay && spawnedEnemies < enemyCant)
+        {
+            SpawnEnemy(spawnPoint, endPoint, waypointList, enemyAggression, enemySpeed);
+            spawnedEnemies++;
+            delayClock = 0f;
+        }
+    }
+
+    private void SpawnEnemy(Transform CspawnPoint, Transform CendPoint, List<Transform> Cwaypoints, float Caggression, float Cspeed)
+    {
+        GameObject cassielInstance = Instantiate(cassiel, CspawnPoint.position, Quaternion.identity);
+        Cassiel cassielComponents = cassielInstance.transform.GetComponent<Cassiel>();
+        cassielComponents.fireRate = Caggression;
+        cassielComponents.speed = Cspeed;
+        cassielComponents.endPoint = CendPoint;
+        cassielComponents.waypoints = Cwaypoints;
     }
 }
