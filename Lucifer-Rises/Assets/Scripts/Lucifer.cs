9c4dd53b43a8ffe66938e1586522d8018bc89902
pancreasio <julianholisu@gmail.com>
2019-06-12 12:24:08 -0300

    Drones can be added and substracted

--- Lucifer-Rises/Assets/Scripts/Lucifer.cs
+++ Lucifer-Rises/Assets/Scripts/Lucifer.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Lucifer : MonoBehaviour
 {
-    public float speedX, speedY, fireRate, maxHP, positionOffset;
-    private float inputX, inputY, fireClock, hp;
-    public GameObject bullet;
+    public float speedX, speedY, fireRate, positionOffset;
+    private float inputX, inputY, fireClock;
+    public int droneAmmount, maxHP;
+    private int  hp;
+    public GameObject bullet, drone1, drone2, drone3, drone4, drone5, drone6;
     private GameObject model;
     public AudioSource fireSound;
     private Transform bounds, cannon1, cannon2, cannon3, cannon4, droneCannon1, droneCannon2, droneCannon3, droneCannon4, droneCannon5, droneCannon6;
@@ -92,6 +94,38 @@ public class Lucifer : MonoBehaviour
             Fire();
             fireClock = 0f;
         }
+
+        if (droneAmmount >= 2)
+        {
+            drone1.SetActive(true);
+            drone2.SetActive(true);
+        }
+        else
+        {
+            drone1.SetActive(false);
+            drone2.SetActive(false);
+        }
+        if (droneAmmount >= 4)
+        {
+            drone3.SetActive(true);
+            drone4.SetActive(true);
+        }
+        else
+        {
+            drone3.SetActive(false);
+            drone4.SetActive(false);
+        }
+        if (droneAmmount >= 6)
+        {
+            drone5.SetActive(true);
+            drone6.SetActive(true);
+        }
+        else
+        {
+            drone5.SetActive(false);
+            drone6.SetActive(false);
+        }
+
     }
 
     private void Fire()
@@ -101,12 +135,21 @@ public class Lucifer : MonoBehaviour
         Instantiate(bullet, cannon2.position, Quaternion.identity);
         Instantiate(bullet, cannon3.position, Quaternion.identity);
         Instantiate(bullet, cannon4.position, Quaternion.identity);
-        Instantiate(bullet, droneCannon1.position, Quaternion.identity);
-        Instantiate(bullet, droneCannon2.position, Quaternion.identity);
-        Instantiate(bullet, droneCannon3.position, Quaternion.identity);
-        Instantiate(bullet, droneCannon4.position, Quaternion.identity);
-        Instantiate(bullet, droneCannon5.position, Quaternion.identity);
-        Instantiate(bullet, droneCannon6.position, Quaternion.identity);
+        if (droneAmmount >= 2)
+        {
+            Instantiate(bullet, droneCannon1.position, Quaternion.identity);
+            Instantiate(bullet, droneCannon2.position, Quaternion.identity);
+        }
+        if (droneAmmount >= 4)
+        {
+            Instantiate(bullet, droneCannon3.position, Quaternion.identity);
+            Instantiate(bullet, droneCannon4.position, Quaternion.identity);
+        }
+        if (droneAmmount >= 6)
+        {
+            Instantiate(bullet, droneCannon5.position, Quaternion.identity);
+            Instantiate(bullet, droneCannon6.position, Quaternion.identity);
+        }
     }
 
 
